25801a867c2dcf15692352156268b22715617edf
faisalprady13 <faisalprady@gmail.com>
2020-06-30 18:43:29 +0200

    Updates

---
+++ precache-manifest.3b4bd63f6bcb4904915a7075951521e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139a3840783c64d1852857ddc6e015c1",
+    "url": "/master-detail-pattern/index.html"
+  },
+  {
+    "revision": "e661638e35ca5b2709fc",
+    "url": "/master-detail-pattern/static/css/main.68c9d796.chunk.css"
+  },
+  {
+    "revision": "f658ed9b7a77554ba7dc",
+    "url": "/master-detail-pattern/static/js/2.31d5a620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/master-detail-pattern/static/js/2.31d5a620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e661638e35ca5b2709fc",
+    "url": "/master-detail-pattern/static/js/main.ba434840.chunk.js"
+  },
+  {
+    "revision": "fbaf648d7f3990eb899f",
+    "url": "/master-detail-pattern/static/js/runtime-main.7ed2b6c0.js"
+  }
+]);
